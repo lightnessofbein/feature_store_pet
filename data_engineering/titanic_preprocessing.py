01e39992b420c1ee153450b331f62df0bbd9f87f
lightnessofbein <lightnessofbein@gmail.com>
2022-07-11 13:17:36 +0300

    changed preprocessing step

--- data_engineering/titanic_preprocessing.py
+++ data_engineering/titanic_preprocessing.py
@@ -1,39 +1,40 @@
-import yaml 
+import argparse
+import yaml
+import joblib
 import pandas as pd
-from os import path
+from os import listdir
 from sklearn.impute import SimpleImputer
 
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OrdinalEncoder
 
 
-def preprocess_raw_file(rawfile_path, pipe, output_path, timestamp):
-    raw_df = pd.read_csv(rawfile_path)
-    preprocessed_df = pd.DataFrame(pipe.fit_transform(raw_df), columns=pipe.get_feature_names_out())
-    preprocessed_df['dummy_timestamp'] = timestamp
-    preprocessed_df.to_csv(output_path, index=False, mode='a', header=not path.exists(output_path))
-    print(f'saved {preprocessed_df.shape[0]} rows to {output_path}')
-
-
 if __name__ == '__main__':
+    print(listdir())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--type', help='train or production processing')
+    args = parser.parse_args()
+
     with open('data_engineering/raw_data/titanic_config.yaml') as f:
         config = yaml.load(f, Loader=yaml.loader.SafeLoader)
 
-    preproc_pipe = Pipeline([
-                             ('sex_encoder', OrdinalEncoder()),
-                             ('imputer', SimpleImputer(strategy='median'))
-                             ],
-                            verbose=True)
+    if args.type == 'train':
+        preproc_pipe = Pipeline([
+                                ('sex_encoder', OrdinalEncoder()),
+                                ('imputer', SimpleImputer(strategy='median'))
+                                ],
+                                verbose=True)
+    elif args.type == 'production':
+        preproc_pipe = joblib.load(config['articat_pipe_path'])
 
-    preprocess_raw_file(rawfile_path=config['train']['path'],
-                        pipe=preproc_pipe,
-                        output_path=config['output_path'],
-                        timestamp=config['train']['timestamp'])
-    print('train data has been ingested!')
+    raw_df = pd.read_csv(config[args.type]['path'])
+    preprocessed_df = pd.DataFrame(preproc_pipe.fit_transform(raw_df), columns=preproc_pipe.get_feature_names_out())
+    preprocessed_df['dummy_timestamp'] = config[args.type]['timestamp']
+    preprocessed_df.to_parquet(config[args.type]['output_path'], index=False)
 
-    for period in config['prod']:
-        preprocess_raw_file(rawfile_path=config['prod'][period]['path'],
-                            pipe=preproc_pipe,
-                            output_path=config['output_path'],
-                            timestamp=config['prod'][period]['timestamp'])
-        print(f'{period} data has been ingested!')
+    # dumping trained pipe so preprocessing is gonna be the same for production data
+    if args.type == 'train':
+        joblib.dump(preproc_pipe, config['articat_pipe_path'])
+        print('pipe was dumped after ingestion')
+
+    print(f'{args.type} data has been ingested!')
