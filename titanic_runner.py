ea019393b1900f10544ea714cde68b616f89991b
lightnessofbein <lightnessofbein@gmail.com>
2022-09-21 12:39:04 +0300

    tested precommit

--- titanic_runner.py
+++ titanic_runner.py
@@ -1,6 +1,6 @@
 import feast
-import yaml
 import pandas as pd
+import yaml
 from joblib import dump
 from sklearn.linear_model import LogisticRegression
 
@@ -9,13 +9,19 @@ entity_df = pd.read_csv("data_engineering/raw_data/titanic_train.csv", usecols=[
 with open("data_engineering/raw_data/titanic_config.yaml") as f:
     config = yaml.load(f, Loader=yaml.loader.SafeLoader)
 entity_df["dummy_timestamp"] = config["train"]["timestamp"]
+
 # Connect to your feature store provider
 fs = feast.FeatureStore(repo_path="data_science_repo/feast_feature_store")
 
 # Retrieve training data from BigQuery
 training_df = fs.get_historical_features(
     entity_df=entity_df,
-    features=["passengers_stats:Survived", "passengers_stats:Pclass", "passengers_stats:Name", "passengers_stats:Sex"],
+    features=[
+        "passengers_stats:Survived",
+        "passengers_stats:Pclass",
+        "passengers_stats:Name",
+        "passengers_stats:Sex",
+    ],
 ).to_df()
 
 print("----- Feature schema -----\n")
@@ -36,7 +42,6 @@ train_X = training_df.drop(columns=entity_df.columns.to_list() + [target_col]).d
 print(f"train dataset size: {train_X.shape[0]}, request size: {entity_df.shape[0]}")
 train_Y = training_df.dropna().loc[:, target_col]
 reg.fit(train_X[sorted(train_X)], train_Y)
-
 
 # Save model
 dump(reg, "driver_model.dump")
